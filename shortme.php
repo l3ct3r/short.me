16f5471ff72c1b5465e08f15513564cce5e6f605
3ncrypt3db0t <kaalratri@tuta.io>
2021-09-15 11:07:51 +0545

    short.me Page

--- shortme.php
+++ shortme.php
@@ -1,162 +1,126 @@
+<?php 
+  include "php/config.php";
+  $new_url = "";
+  if(isset($_GET)){
+    foreach($_GET as $key=>$val){
+      $u = mysqli_real_escape_string($conn, $key);
+      $new_url = str_replace('/', '', $u);
+    }
+      $sql = mysqli_query($conn, "SELECT full_url FROM url WHERE shorten_url = '{$new_url}'");
+      if(mysqli_num_rows($sql) > 0){
+        $sql2 = mysqli_query($conn, "UPDATE url SET clicks = clicks + 1 WHERE shorten_url = '{$new_url}'");
+        if($sql2){
+            $full_url = mysqli_fetch_assoc($sql);
+            header("Location:".$full_url['full_url']);
+          }
+      }
+  }
+?>
+<!-- PHP Blocks ENDS  -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <title>short.me | URL Shortener</title>
 
-    <!-- ICON SCOUT CDN -->
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+  <!-- TITLE ICON -->
+  <link rel="shortcut icon" href="assets/icons/link.png" type="image/x-icon">
 
-    <!-- STYLE SHEET -->
-    <link rel="stylesheet" href="assets/css/styles.css">
+  <!-- STYLE SHEET -->
+  <link rel="stylesheet" href="assets/css/styles.css">
 
-    <title>Short.me | Minimal URL Shortener</title>
+  <!-- Icon Scout Link for Icons -->
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css">
 
-    <!-- TITLE ICON -->
-    <link rel="shortcut icon" href="assets/icons/link.png" type="image/x-icon">
 </head>
 <body>
     <!-- WRAPPER STARTS -->
-    <div class="wrapper">
-        <form action="#">
-            <input type="text" name="full-url" placeholder="Enter OR paste your long URL" required>
-            <i class="url-icon uil uil-link"></i>
-            <button>Shorten</button>
-        </form>
+  <div class="wrapper">
+    <form action="#" autocomplete="off">
+      <input type="text" spellcheck="false" name="full_url" placeholder="Enter or paste a long url" required>
+      <i class="url-icon uil uil-link"></i>
+      <button>Shorten</button>
+    </form>
+    <?php
+      $sql2 = mysqli_query($conn, "SELECT * FROM url ORDER BY id DESC");
+      if(mysqli_num_rows($sql2) > 0){;
+        ?>
+          <div class="statistics">
+            <?php
+              $sql3 = mysqli_query($conn, "SELECT COUNT(*) FROM url");
+              $res = mysqli_fetch_assoc($sql3);
 
-        <!-- COUNT STARTS -->
-        <div class="count">
-            <span>Total Links: <span>10</span> & Total Clicks: 140</span>
-            <a href="#">Clear All</a>
+              $sql4 = mysqli_query($conn, "SELECT clicks FROM url");
+              $total = 0;
+              while($count = mysqli_fetch_assoc($sql4)){
+                $total = $count['clicks'] + $total;
+              }
+            ?>
+            <span>Total Links: <span><?php echo end($res) ?></span> & Total Clicks: <span><?php echo $total ?></span></span>
+            <a href="php/delete.php?delete=all">Clear All</a>
         </div>
-        <!-- COUNT ENDS -->
-
-        <!-- URL AREA STARTS -->
         <div class="urls-area">
-            <!-- Title Starts -->
-            <div class="title">
-                <li>Shorten URL</li>
-                <li>Original URL</li>
-                <li>Clicks</li>
-                <li>Action</li>
-            </div>
-            <!-- Title Ends -->
-            
-            <!-- Data Starts -->
-            <div class="data">
-                <li><a href="#">example.com/abx64</a></li>
-                <li>https://www.github.com/3ncrypt3db0t</li>
-                <li>10</li>
-                <li><a href="#">Delete</a></li>
-            </div>
+          <div class="title">
+            <li>Shorten URL</li>
+            <li>Original URL</li>
+            <li>Clicks</li>
+            <li>Action</li>
+          </div>
+          <?php
+            while($row = mysqli_fetch_assoc($sql2)){
+              ?>
+                <div class="data">
+                <li>
+                  <a href="<?php echo $domain.$row['shorten_url'] ?>" target="_blank">
+                  <?php
+                    if($domain.strlen($row['shorten_url']) > 50){
+                      echo $domain.substr($row['shorten_url'], 0, 50) . '...';
+                    }else{
+                      echo $domain.$row['shorten_url'];
+                    }
+                  ?>
+                  </a>
+                </li> 
+                <li>
+                  <?php
+                    if(strlen($row['full_url']) > 60){
+                      echo substr($row['full_url'], 0, 60) . '...';
+                    }else{
+                      echo $row['full_url'];
+                    }
+                  ?>
+                </li> 
+              </li>
+                <li><?php echo $row['clicks'] ?></li>
+                <li><a href="php/delete.php?id=<?php echo $row['shorten_url'] ?>">Delete</a></li>
+              </div>
+              <?php
+            }
+          ?>
+      </div>
+        <?php
+      }
+    ?>
+  </div>
+  <!-- WRAPPER ENDS -->
 
-            <div class="data">
-                <li><a href="#">example.com/abx64</a></li>
-                <li>https://www.github.com/3ncrypt3db0t</li>
-                <li>10</li>
-                <li><a href="#">Delete</a></li>
-            </div>
+  <!-- BLUR EFFECTS -->
+  <div class="blur-effect"></div>
 
-            <div class="data">
-                <li><a href="#">example.com/abx64</a></li>
-                <li>https://www.github.com/3ncrypt3db0t</li>
-                <li>10</li>
-                <li><a href="#">Delete</a></li>
-            </div>
+  <!-- POP UP BOX STARTS -->
+  <div class="popup-box">
+  <div class="info-box">Your short link is ready. Link edited and saved once can't be edited again.</div>
+  <form action="#" autocomplete="off">
+    <label>Edit your shorten url</label>
+    <input type="text" class="shorten-url" spellcheck="false" required>
+    <i class="copy-icon uil uil-copy-alt"></i>
+    <button>Save</button>
+  </form>
+  </div>
 
-            <div class="data">
-                <li><a href="#">example.com/abx64</a></li>
-                <li>https://www.github.com/3ncrypt3db0t</li>
-                <li>10</li>
-                <li><a href="#">Delete</a></li>
-            </div>
+  <!-- MAIN JS -->
+  <script src="assets/js/script.js"></script>
 
-            <div class="data">
-                <li><a href="#">example.com/abx64</a></li>
-                <li>https://www.github.com/3ncrypt3db0t</li>
-                <li>10</li>
-                <li><a href="#">Delete</a></li>
-            </div>
-
-            <div class="data">
-                <li><a href="#">example.com/abx64</a></li>
-                <li>https://www.github.com/3ncrypt3db0t</li>
-                <li>10</li>
-                <li><a href="#">Delete</a></li>
-            </div>
-
-            <div class="data">
-                <li><a href="#">example.com/abx64</a></li>
-                <li>https://www.github.com/3ncrypt3db0t</li>
-                <li>10</li>
-                <li><a href="#">Delete</a></li>
-            </div>
-
-            <div class="data">
-                <li><a href="#">example.com/abx64</a></li>
-                <li>https://www.github.com/3ncrypt3db0t</li>
-                <li>10</li>
-                <li><a href="#">Delete</a></li>
-            </div>
-
-            <div class="data">
-                <li><a href="#">example.com/abx64</a></li>
-                <li>https://www.github.com/3ncrypt3db0t</li>
-                <li>10</li>
-                <li><a href="#">Delete</a></li>
-            </div>
-
-            <div class="data">
-                <li><a href="#">example.com/abx64</a></li>
-                <li>https://www.github.com/3ncrypt3db0t</li>
-                <li>10</li>
-                <li><a href="#">Delete</a></li>
-            </div>
-
-            <div class="data">
-                <li><a href="#">example.com/abx64</a></li>
-                <li>https://www.github.com/3ncrypt3db0t</li>
-                <li>10</li>
-                <li><a href="#">Delete</a></li>
-            </div>
-
-            <div class="data">
-                <li><a href="#">example.com/abx64</a></li>
-                <li>https://www.github.com/3ncrypt3db0t</li>
-                <li>10</li>
-                <li><a href="#">Delete</a></li>
-            </div>
-
-            <div class="data">
-                <li><a href="#">example.com/abx64</a></li>
-                <li>https://www.github.com/3ncrypt3db0t</li>
-                <li>10</li>
-                <li><a href="#">Delete</a></li>
-            </div>
-
-            <!-- Data Ends -->
-        </div>
-        <!-- URL AREA ENDS -->
-    </div>
-    <!-- WRAPPER ENDS -->
-
-    <!-- BLUR EFFECTS -->
-    <div class="blur-effect"></div>
-
-    <!-- PopUp STARTS -->
-    <div class="popup-box">
-        <div class="info-box">Edit your short link now. Once edited and saved file can't be edited again.</div>
-        <form action="#">
-            <label>Edit your shortened URL:</label>
-            <input type="text" value="example.com/abcx64" spellcheck="false">
-            <i class="copy-icon uil uil-copy-alt"></i>
-            <button>Save</button>
-        </form>
-    </div>
-    <!-- PopUp ENDS -->
-    
-    <!-- MAIN JS -->
-    <script src="assets/js/script.js"></script>
 </body>
 </html>
