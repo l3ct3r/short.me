0fa9ff1f01e57ccf804a806faed4b0a9d5671bb0
3ncrypt3db0t <kaalratri@tuta.io>
2021-09-15 10:31:34 +0545

    PHP | URL Control

--- php/url-control.php
+++ php/url-control.php
@@ -6,7 +6,27 @@
     // if the full url box is not empty and the user entered url is valid url
     if(!empty($full_url) && filter_var($full_url, FILTER_VALIDATE_URL)) {
         $ran_url = substr(md5(microtime()), rand(0, 26), 5); // generating random 5 characters URL 
-        //$sql = mysqli_query($conn, "SELECT ");
+        
+        // checking that random generated url already exists in the database or not
+        $sql = mysqli_query($conn, "SELECT shorten_url FROM url WHERE shorten_url= {ran_url}");
+        if(mysqli_num_rows($sql) > 0) {
+            echo "Something went wrong. Please regenerate the URL again";
+        } else {
+            // let's insert user type URL into table with short URL 
+            $sql2 = mysqli_query($conn, "INSERT INTO url (shorten_url, full_url, clicks)
+                                            VALUES ('(ran_url)', '{$full_url}', '0')");
+
+            if($sql2) { // if data inserted is successful 
+                // something recently inserted short_link/URL
+                $sql3 = mysqli_query($conn, "SELECT shorten_url FROM url WHERE shorten_url = {ran_url}");
+                if(mysqli_num_rows($sql) > 0) {
+                    $shorten_url = mysqli_fetch_assoc($sql3);
+                    echo $shorten_url['shorten_url'];
+                }
+            } else {
+                echo "Something went wrong! Please insert again";
+            }
+        }
     } else {
         echo "$full_url - This is not a valid URL !";
     }
