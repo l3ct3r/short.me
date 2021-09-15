b3f6d1e61b91c0a52e06345715d44730608cc6f2
3ncrypt3db0t <kaalratri@tuta.io>
2021-09-15 11:07:17 +0545

    PHP | Config

--- php/config.php
+++ php/config.php
@@ -1,6 +1,12 @@
-<?php
-    $conn = mysqli_connect("localhost", "root", "", "shortMe");
-    if(!$conn) { // if database is not connected
-        echo "DataBase connection error".mysqli_connect_error();
+<?php 
+    $domain = "localhost:8080/short.me/shortme.php";
+    $host = "localhost";
+    $user = "root"; //Database username
+    $pass = ""; //Database password
+    $db = "shortMe"; //Database name
+
+    $conn = mysqli_connect($host, $user, $pass, $db);
+    if(!$conn){
+        echo "Database connection error".mysqli_connect_error();
     }
 ?>
