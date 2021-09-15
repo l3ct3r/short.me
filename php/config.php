7a907f427962f3b13c7df0b3f7ff8fbe98de4ec0
3ncrypt3db0t <kaalratri@tuta.io>
2021-09-15 12:10:24 +0545

    Deleted Old Files

--- php/config.php
+++ php/config.php
@@ -1,5 +1,13 @@
 <?php 
-    $domain = "localhost:8080/short.me/shortme.php";
+    /* if you're working on localhost then you don't need to change anything
+       but if you're thinking to upload it to live server then you've to edit something
+    1. Paste your website url with forward slash(/) in the domain variable, you don't need 
+       to write https://wwww. before the domain name if you have domain redirection setup
+    2. Change user, pass, db values accordingly mentioned in the comments below
+    3. Go to JavaScript file and search this keyword - let domain - then paste your url there 
+    4. After all changes you've to wait because javascript file save changes may take time to reflect */ 
+
+    $domain = "localhost/url/";
     $host = "localhost";
     $user = "root"; //Database username
     $pass = ""; //Database password
