fddc834bd346519df664dc4038bceb77a257731f
3ncrypt3db0t <kaalratri@tuta.io>
2021-09-15 10:13:15 +0545

    PHP | DB Config

--- php/config.php
+++ php/config.php
@@ -1,3 +1,6 @@
 <?php
-    $conn = mysqli_connect("localhost", "root", "", "");
+    $conn = mysqli_connect("localhost", "root", "", "shortMe");
+    if(!$conn) { // if database is not connected
+        echo "DataBase connection error".mysqli_connect_error();
+    }
 ?>
